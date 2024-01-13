d347c7f2ee6644d74e651d1093be2ac944a58a55
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-01-13 23:56:56 +0100

    Create valid SDF file (absolute paths)

--- core/sdf_exporter.py
+++ core/sdf_exporter.py
@@ -6,20 +6,6 @@ import os
 
 def export_mesh(filepath: str):
     ''' Exports the dae file and its associated textures of the selected objects '''
-    # bpy.ops.wm.collada_export(
-    #     filepath=filepath,
-    #     check_existing=False,
-    #     filter_collada=True,
-    #     filter_folder=True,
-    #     apply_modifiers=True,
-    #     export_global_forward_selection='X',
-    #     export_global_up_selection='Z',
-    #     filemode=8,
-    #     use_object_instantiation=False,
-    #     limit_precision=True,
-    #     selected=True,
-    # )
-    # filepath = '/tmp/cheval.obj'
     bpy.ops.wm.obj_export(
         filepath=filepath + '.obj',
         check_existing=False,
@@ -31,24 +17,38 @@ def export_mesh(filepath: str):
     )
 
 
-def export_images(filepath: str):
-    for image in bpy.data.images:
-        if image.has_data:
-            image.save(filepath=os.path.join(filepath, image.name))
+def export_image(name: str, dirname: str):
+    image_path = os.path.join(dirname, name)
+
+    if not os.path.exists(image_path):
+        image = bpy.data.images[name]
+        image.save(filepath=image_path)
 
 
-def create_sdf_link(model: ET.Element, object: bpy.types.Object):
-    link = ET.SubElement(model, "link", attrib={"name": object.name})
-    visual = ET.SubElement(link, "visual", attrib={"name": object.name})
+def append_ogre_material(name: str, material_filepath: str, image_filename: str):
+    with open(material_filepath, 'a') as file:
+        file.write(f'''\
+material {name}
+{{
+  technique
+  {{
+    pass
+    {{
+      cull_hardware none
+      cull_software none
 
-    geometry = ET.SubElement(visual, "geometry")
-    mesh = ET.SubElement(geometry, "mesh")
-    uri = ET.SubElement(mesh, "uri")
-    uri.text = object.name + '.obj'
-    submesh = ET.SubElement(mesh, "submesh")
-    submesh_name = ET.SubElement(submesh, "name")
-    submesh_name.text = object.name
+      texture_unit
+      {{
+        texture {image_filename}
+      }}
+    }}
+  }}
+}}
 
+''')
+
+
+def create_sdf_material(visual: ET.Element, object: bpy.types.Object, filepath: str):
     # grab diffuse/albedo map
     diffuse_map = None
     if object.active_material and object.active_material.node_tree:
@@ -56,34 +56,50 @@ def create_sdf_link(model: ET.Element, object: bpy.types.Object):
         principled = next(n for n in nodes if n.type == 'BSDF_PRINCIPLED')
         if principled is not None:
             base_color = principled.inputs['Base Color']  # Or principled.inputs[0]
-            # value = base_color.default_value
             if len(base_color.links):
                 link_node = base_color.links[0].from_node
                 diffuse_map = link_node.image.name
 
+    material_filepath = os.path.join(filepath, object.name + '.material')
+
+    export_image(diffuse_map, filepath)
+    append_ogre_material(object.name, material_filepath, diffuse_map)
+
     # setup diffuse/specular color
     material = ET.SubElement(visual, "material")
-    diffuse = ET.SubElement(material, "diffuse")
-    diffuse.text = "1.0 1.0 1.0 1.0"
-    specular = ET.SubElement(material, "specular")
-    specular.text = "0.0 0.0 0.0 1.0"
-    pbr = ET.SubElement(material, "pbr")
-    metal = ET.SubElement(pbr, "metal")
-    if diffuse_map:
-        albedo_map = ET.SubElement(metal, "albedo_map")
-        albedo_map.text = os.path.join('meshes', diffuse_map)
+    script = ET.SubElement(material, 'script')
+    ET.SubElement(script, 'uri').text = material_filepath
+    ET.SubElement(script, 'name').text = object.name
+
+
+def create_sdf_link(
+    model: ET.Element,
+    object: bpy.types.Object,
+    meshes_path: str,
+    material_path: str,
+):
+    mesh_uri = os.path.join(meshes_path, object.name + '.obj')
+
+    link = ET.SubElement(model, "link", attrib={"name": object.name})
+
+    visual = ET.SubElement(link, "visual", attrib={"name": object.name})
+    geometry = ET.SubElement(visual, "geometry")
+    mesh = ET.SubElement(geometry, "mesh")
+    ET.SubElement(mesh, "uri").text = mesh_uri
+
+    create_sdf_material(visual, object, material_path)
 
     # sdf collision tags
     collision = ET.SubElement(link, "collision", attrib={"name": "collision"})
     geometry = ET.SubElement(collision, "geometry")
     mesh = ET.SubElement(geometry, "mesh")
-    uri = ET.SubElement(mesh, "uri")
-    uri.text = object.name
+    ET.SubElement(mesh, "uri").text = mesh_uri
 
-    # surface = ET.SubElement(collision, "surface")
-    # contact = ET.SubElement(collision, "contact")
-    collide_bitmask = ET.SubElement(collision, "collide_bitmask")
-    collide_bitmask.text = "0x01"
+    surface = ET.SubElement(collision, "surface")
+    contact = ET.SubElement(surface, "contact")
+    ET.SubElement(contact, "collide_without_contact").text = 'true'
+    ET.SubElement(contact, "collide_without_contact_bitmask").text = '0x01'
+    ET.SubElement(contact, "collide_bitmask").text = '0x00'
 
 
 def create_config(model_name: str, sdf_filename: str, author_name: str):
@@ -106,33 +122,26 @@ def export_sdf(path: str, name: str, collection: bpy.types.Collection, author: s
     sdf_filename = 'model.sdf'
     model_config_filename = 'model.config'
     meshes_path = os.path.join(path, 'meshes')
+    materials_path = os.path.join(path, 'materials')
 
     os.makedirs(meshes_path, exist_ok=True)
+    os.makedirs(materials_path, exist_ok=True)
 
     if not author:
         author = 'Generated by cropcraft'
 
-    mesh_objects = []
+    # export sdf xml based off the scene
+    sdf = ET.Element('sdf', attrib={'version': '1.7'})
+
+    model = ET.SubElement(sdf, "model", attrib={"name": name})
+    ET.SubElement(model, "static").text = 'true'
 
     for object in collection.all_objects.values():
         if object.type == 'MESH':
             bpy.ops.object.select_all(action='DESELECT')
             object.select_set(True)
             export_mesh(os.path.join(meshes_path, object.name))
-            mesh_objects.append(object)
-
-    export_images(meshes_path)
-
-    # export sdf xml based off the scene
-    sdf = ET.Element('sdf', attrib={'version': '1.7'})
-
-    model = ET.SubElement(sdf, "model", attrib={"name": name})
-    static = ET.SubElement(sdf, "static")
-    static.text = "true"
-
-    # for each object, create a link
-    for object in mesh_objects:
-        create_sdf_link(model, object)
+            create_sdf_link(model, object, meshes_path, materials_path)
 
     # sdf write to file
     xml_string = ET.tostring(sdf, encoding='unicode')
