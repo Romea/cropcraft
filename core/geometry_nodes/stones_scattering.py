12413697a0780640d3c2e44f030c1db50d5ddb1f
Cyrille Pierre <cyrille.pierre@inrae.fr>
2024-02-20 09:57:23 +0100

    Add license

--- core/geometry_nodes/stones_scattering.py
+++ core/geometry_nodes/stones_scattering.py
@@ -1,3 +1,15 @@
+# Copyright 2024 INRAE, French National Research Institute for Agriculture, Food and Environment
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import bpy
 
 def stones_scattering_node_group():
